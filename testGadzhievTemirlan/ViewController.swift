75f5f5e14ab4962e6ccf48dccf7bba85805e480c
tommypanda10 <tem.gadzhiev@mail.ru>
2022-11-07 14:49:42 +0300

    new

--- testGadzhievTemirlan/ViewController.swift
+++ testGadzhievTemirlan/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var clickCount = 0
+    @IBOutlet weak var counterValue: UILabel!
+    @IBOutlet weak var buttonTap: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterValue.text = "Значение счетчика"
+        
     }
 
-
+    @IBAction func buttonCount(_ sender: Any) {
+        clickCount += 1
+        counterValue.text = "Значение счетчика : \(clickCount)"
+    }
+    
 }
 
